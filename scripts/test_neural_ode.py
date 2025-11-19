782b4e34419a414004ff713f06ba298fdcacc729
Mehdi Tafaroji <mtafaroji@gmail.com>
2025-11-18 23:37:13 -0600

    ِDelete Extra Files

--- scripts/test_neural_ode.py
+++ scripts/test_neural_ode.py
@@ -2,31 +2,44 @@ import torch
 import matplotlib.pyplot as plt
 from torchdiffeq import odeint
 
-
 import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parents[1]))
 
 
-from models.f_theta1 import FTheta
+from models.f_theta import FTheta
+
+
+# ------------------- Device -----------------------
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("Using device:", device)
+
+
 
 # ---------- Load trained model ----------
 model_path = "models/neural_ode_model.pth"
+
 dataset_path = "data/processed/dataset.pt"
 
 # Load dataset
 dataset = torch.load(dataset_path)
-data = dataset['data']
-time = dataset['time']
+data = dataset['data'].float().to(device)
+time = dataset['time'].float().to(device)
+
+
+
 
 num_runs, num_steps, num_features = data.shape
 
-
-h_true = data[4]
-h0 = h_true[0]
+########################################
+########################################
+h_true = data[1]
+h0 = h_true[0].unsqueeze(0)  # shape: (1, num_features)
+########################################
+########################################
 
 # Load model
-f_theta = FTheta(input_dim=num_features)
+f_theta = FTheta(input_dim=num_features).to(device)
 f_theta.load_state_dict(torch.load(model_path))
 f_theta.eval()
 
@@ -35,14 +48,24 @@ f_theta.eval()
 
 # ---------- Predict the trajectory ----------
 with torch.no_grad():
-    h_pred = odeint(f_theta, h0, time)  # shape: (num_steps, num_features)
+    #h_pred = odeint(f_theta, h0, time)  # shape: (num_steps, num_features)
+
+    h_pred = odeint(f_theta, h0, time, method='rk4')
+    h_pred = h_pred.squeeze(1)
+
+
+
+h_true = h_true.cpu()
+h_pred = h_pred.cpu()
+time_cpu = time.cpu()
+
 
 # ---------- Plot comparison ----------
 plt.figure(figsize=(10, 6))
 for i in range(num_features):
     plt.subplot(num_features, 1, i+1)
-    plt.plot(time.numpy(), h_true[:, i].numpy(), 'b-', label=f"True Stock {i+1}")
-    plt.plot(time.numpy(), h_pred[:, i].numpy(), 'r--', label=f"Predicted Stock {i+1}")
+    plt.plot(time_cpu.numpy(), h_true[:, i].numpy(), 'b-', label=f"True Stock {i+1}")
+    plt.plot(time_cpu.numpy(), h_pred[:, i].numpy(), 'r--', label=f"Predicted Stock {i+1}")
     plt.legend()
     plt.xlabel("Time")
     plt.ylabel("Value")
--- scripts/train_neural_ode.py
+++ scripts/train_neural_ode.py
@@ -2,47 +2,56 @@ import torch
 import torch.nn as nn
 from torchdiffeq import odeint
 from pathlib import Path
-
 import sys
-from pathlib import Path
+
+#from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parents[1]))
 
-
 # Import our model
-from models.f_theta1 import FTheta
+from models.f_theta import FTheta
+
+
+
+# ------------------- Device -----------------------
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("Using device:", device)
+
 
 # ------- Load processed data ---------------------
 dataset = torch.load("data/processed/dataset.pt")
-data = dataset['data']      
-time = dataset['time']      
+data = dataset['data'].float().to(device)       # shape: (num_runs, time_steps, features)     
+time = dataset['time'].float().to(device)       # shape: (time_steps,)  
+
+
 
 num_runs, num_steps, num_features = data.shape
 
 # ---------- Initialize model, optimizer, and loss ----------
-f_theta = FTheta(input_dim=num_features)
+f_theta = FTheta(input_dim=num_features).to(device)
 optimizer = torch.optim.Adam(f_theta.parameters(), lr=1e-3)
 loss_fn = nn.MSELoss()
 
 # ----------Training loop ------------------------------------
-num_epochs = 40
+
+num_epochs = 400
 for epoch in range(num_epochs):
     total_loss = 0.0
 
-    for i in range(num_runs):
-        h_true = data[i]                   
-        h0 = h_true[0]       
+    batch_size = 8
+    for start in range(0, num_runs, batch_size):
+        end = start + batch_size
+        batch = data[start:end]
+        h0_batch = batch[:, 0, :]
+   
 
         # Predict trajectory
 
-        #def func(t, h):
-        #    return f_theta(h, t)
-
-        h_pred = odeint(f_theta, h0, time)
-
+        h_pred = odeint(f_theta, h0_batch, time, method='rk4')
+        h_pred = h_pred.permute(1, 0, 2) 
       
 
         # Compute loss
-        loss = loss_fn(h_pred, h_true)
+        loss = loss_fn(h_pred, batch)
 
         # Backpropagation
         optimizer.zero_grad()
